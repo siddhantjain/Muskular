ba56961bb5eb67a615231b1554fdc3eec63b9a31
akash.jatangi <D3lleGr1ty!>
2015-12-06 22:37:31 +0530

    SHaredPref edited

--- app/src/main/java/com/siddhantjain/muskular/utils/DataStore.java
+++ app/src/main/java/com/siddhantjain/muskular/utils/DataStore.java
@@ -10,11 +10,28 @@ import com.siddhantjain.muskular.R;
  */
 public class DataStore {
     public static SharedPreferences userProfileStore = null;
+    private static Context mContext;
+
+    private DataStore() {} //Prevent Instantiation
 
     public static SharedPreferences getUserProfileStore(Context mContext){
         if (userProfileStore ==null){
             userProfileStore = mContext.getSharedPreferences(mContext.getString(R.string.preference_file_key_user_profile),Context.MODE_PRIVATE);
         }
         return userProfileStore;
+    }
+    public static void init(Context mContextArg){
+        if (userProfileStore ==null){
+            userProfileStore = mContextArg.getSharedPreferences(mContextArg.getString(R.string.preference_file_key_user_profile),Context.MODE_PRIVATE);
+            mContext = mContextArg;
+        }
+    }
+    public static String getUserId(){
+        return userProfileStore.getString(mContext.getString(R.string.user_id),null);
+    }
+    public static void setUserId(String user_id){
+        SharedPreferences.Editor SPEditor = userProfileStore.edit();
+        SPEditor.putString(mContext.getString(R.string.user_id),user_id);
+        SPEditor.commit();
     }
 }
